a9740067365b1405e321de07468dd63598add425
reinerBa <4329883+reinerBa@users.noreply.github.com>
2024-01-14 14:36:50 +0100

    fetch url only when node is version 20 or above

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { ConfigEnv, UserConfigExport } from 'vite'
 import stopdev, {stopdevOptions} from './src'
 import { server } from 'typescript'
 
+const [major, minor, patch] = process.versions.node.split('.').map(Number)
 let beforeStopWorks = false
 let afterStopWorks = true
 let port = 5173
@@ -10,9 +11,12 @@ const plugins =[
   stopdev({
     afterIdle: 6000, 
     beforeStop: async () => {
-      const r = await fetch(`http://localhost:${port}`)
-      const text = await r.text()
-      console.log(`fetched html: ${text.substring(0, 15)}`)
+      if(major >= 20) {
+
+        const r = await fetch(`http://localhost:${port}`)
+        const text = await r.text()
+        console.log(`fetched html: ${text.substring(0, 15)}`)
+      }
 
       console.log('goint to stop vite dev server')
       beforeStopWorks = true   
